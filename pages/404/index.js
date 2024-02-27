bad9c1d87288354c88db185a016b7401125a03fe
Nicholas V <nvfastplease@gmail.com>
2024-02-27 08:52:18 -0800

    this is a dev commit: 11 files changed, 104 insertions(+), 5 deletion… (#1)
    * this is a dev commit: 11 files changed, 104 insertions(+), 5 deletions(-)
    
     .github/xworkflowsx/deploy.yml (new)               | 64 ++++++++++++++++++++++
    
     _config.yml                                        |  5 --
    
     pages/404/index.html                               | 22 ++++++++
    
     pages/404/index.js (new)                           | 10 ++++
    
     pages/shared_scripts/constants.js (new)            |  2 +
    
     pages/shared_scripts/default_script/index.js (new) |  1 +
    
     pages/shared_scripts/entry_point/index.js (new)    |  1 +
    
     pages/shared_scripts/stk/constants.js (new)        |  1 +
    
     pages/shared_scripts/stk/head/index.js (new)       |  1 +
    
     pages/shared_scripts/stk/index.js (new)            |  1 +
    
     pages/shared_scripts/stk/utils.js (new)            |  1 +
    
    message generated by QWD (github.com/nvfp/qwd)
    
    * this is a dev commit: 1 file changed, 0 insertions(+), 0 deletions(-)
    
     .github/{xworkflowsx => workflows}/deploy.yml | 0
    
    message generated by QWD (github.com/nvfp/qwd)

---
+++ pages/404/index.js
@@ -0,0 +1,10 @@
+import entryPoint from "../shared_scripts/entry_point/index.js";
+import { addAct } from "../shared_scripts/stk/index.js";
+
+entryPoint(() => {
+    const dst = "https://nvalentinus.com" + window.location.pathname + window.location.search;
+    addAct(`Redirect ${window.location.href}  ->  ${dst}`)
+    setTimeout(() => {
+        window.location.href = dst;
+    }, 123);
+});
