66e4ce474e00388a7a23195069abbe1550212a73
Nicholas Valentinus <nvfastplease@gmail.com>
2024-02-27 09:37:29 -0800

    this is a dev commit: 2 files changed, 144 insertions(+), 35 deletions(-)
     .github/workflows/deploy.yml    | 169 +++++++++++++++++++++++++++++++---------
    
     pages/the_301_redirect.js (new) |  10 +++
    
    message generated by QWD (github.com/nvfp/qwd)

---
+++ pages/the_301_redirect.js
@@ -0,0 +1,10 @@
+import entryPoint from "../shared_scripts/entry_point/index.js";
+import { addAct } from "../shared_scripts/stk/index.js";
+
+entryPoint(() => {
+    const dst = document.getElementById("for_js_script").value;
+    addAct(`Redirect301: ${window.location.href}  ->  ${dst}`)
+    setTimeout(() => {
+        window.location.href = dst;
+    }, 123);
+});
